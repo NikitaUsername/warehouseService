909cf46e46494eba4228d69a5eb29c5005a5d918
Nikita <nikita.kalyaev@gmail.com>
2019-10-18 18:37:40 +0300

    logging added

--- app/routes.js
+++ app/routes.js
@@ -1,15 +1,22 @@
 var ObjectID = require('mongodb').ObjectID;
+const log4js = require('log4js');
+log4js.configure({
+    appenders: { fileAppender: { type: 'file', filename: 'events.log' } },
+    categories: { default: { appenders: ['fileAppender'], level: 'info' } }
+  });
+const logger = log4js.getLogger('server');
 module.exports = function(app, db){
 
 
     app.post('/warehouse/items', (req, res) => {
-        // Здесь будем создавать заметку.
         const note = { name: req.body.name, amount: +req.body.amount, price: +req.body.price };
         db.collection('warehousecollection').insert(note, (err,result) => {
             if(err) {
                 res.send({ 'error': 'an error has occured' });
+                logger.error('не удалось добавить данные в бд');
             } else {
                 res.send (result.ops[0]);
+                logger.info('данные успешно добавлены в базу');
             }
         }); 
     });
@@ -20,8 +27,10 @@ module.exports = function(app, db){
         db.collection('warehousecollection').findOne(info, (err, item) => {
             if(err) {
                 res.send({ 'error': 'an error has occured' });
+                logger.error('не удалось получить данные по id');
             } else {
                 res.send (item);
+                logger.info('данные по id получены успешно');
             }
         });
     });
@@ -30,8 +39,10 @@ module.exports = function(app, db){
         db.collection('warehousecollection').find({}).toArray((err, result) => {
             if(err) {
                 res.send({ 'error': 'an error has occured' });
+                logger.error('не удалось получить данные о товарах');
             } else {
                 res.send (result);
+                logger.info('данные о всех товарах на складе получены успешно');
             }
         });
     });
@@ -44,12 +55,14 @@ module.exports = function(app, db){
         db.collection('warehousecollection').update( info, {$inc: { amount: +amount } }, (err, item) => {
             if(err) {
                 res.send({ 'error': 'an error has occured' });
+                logger.error('не удалось изменить данные о товаре');
             } else {
                 db.collection('warehousecollection').findOne(info, (err, item) => {
                     if(err) {
                         res.send({ 'error': 'an error has occured' });
                     } else {
                     res.send(item);
+                    logger.info('количество товаров успешно изменено');
                     }
                 });
             }
